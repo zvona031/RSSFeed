1af24060187c4bf724170abd1b081881b4a1cc8e
Zvonimir Pavlović <zvona031@gmail.com>
2024-12-20 20:57:06 +0100

    chore: add Xcode project

---
+++ RSSFeed/RSSFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSSFeedApp.swift
+//  RSSFeed
+//
+//  Created by Zvonimir Pavlović on 20.12.2024..
+//
+
+import SwiftUI
+
+@main
+struct RSSFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
