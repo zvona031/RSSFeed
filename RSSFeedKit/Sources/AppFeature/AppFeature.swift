1cd39535d6c6fa05b4242f63cda986d9429c9a36
Zvonimir Pavlović <zvona031@gmail.com>
2024-12-20 23:59:48 +0100

    feat: add AppFeature and Tab enum, add TabView to the AppView

--- RSSFeedKit/Sources/AppFeature/AppFeature.swift
+++ RSSFeedKit/Sources/AppFeature/AppFeature.swift
@@ -1,1 +1,29 @@
+import ComposableArchitecture
 
+@Reducer
+struct AppFeature {
+
+    @ObservableState
+    struct State {
+        var tab: Tab
+
+        init(tab: Tab = .feeds) {
+            self.tab = tab
+        }
+    }
+
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+    }
+
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+    }
+}
+
+extension AppFeature {
+    enum Tab {
+        case feeds
+        case favorites
+    }
+}
--- RSSFeedKit/Sources/AppFeature/AppView.swift
+++ RSSFeedKit/Sources/AppFeature/AppView.swift
@@ -1,17 +1,41 @@
+import ComposableArchitecture
 import SwiftUI
 
 public struct AppView: View {
+    @Perception.Bindable private var store: StoreOf<AppFeature>
 
-    public init() {}
+    public init() {
+        self.store = Store(initialState: AppFeature.State(), reducer: {
+            AppFeature()
+        })
+    }
 
     public var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        WithPerceptionTracking {
+            TabView(selection: $store.tab) {
+                NavigationStack() {
+                    Text("Feeds")
+                        .navigationTitle("Feeds")
+                }
+                .tabItem {
+                    Text("Feeds tab")
+                    Image(systemName: "newspaper.fill")
+                        .renderingMode(.template)
+                }
+                .tag(AppFeature.Tab.feeds)
+
+                NavigationStack() {
+                    Text("Favorites")
+                        .navigationTitle("Favorites")
+                }
+                .tabItem {
+                    Text("Favorites tab")
+                    Image(systemName: "star.fill")
+                        .renderingMode(.template)
+                }
+                .tag(AppFeature.Tab.favorites)
+            }
         }
-        .padding()
     }
 }
 
