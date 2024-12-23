5e895e531311313dac17ef75eee847a16e0072c2
Zvonimir Pavlović <zvona031@gmail.com>
2024-12-23 21:52:41 +0100

    chore: change id from UUID to the url, make imageUrl optional

--- RSSFeedKit/Sources/Domain/RSSFeed.swift
+++ RSSFeedKit/Sources/Domain/RSSFeed.swift
@@ -1,10 +1,26 @@
 import Foundation
 
 public struct RSSFeed: Identifiable {
-    public let id: UUID
+    public var id: URL {
+        url
+    }
     public let url: URL
     public let name: String
     public let description: String
-    public let imageUrl: URL
+    public let imageUrl: URL?
     public let items: [Item]
+
+    public init(
+        url: URL,
+        name: String,
+        description: String,
+        imageUrl: URL?,
+        items: [Item]
+    ) {
+        self.url = url
+        self.name = name
+        self.description = description
+        self.imageUrl = imageUrl
+        self.items = items
+    }
 }
