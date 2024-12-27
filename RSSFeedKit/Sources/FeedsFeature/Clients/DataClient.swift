5e54b25acb8fa07ebe9f74942c21362719d2d218
Zvonimir Pavlović <zvona031@gmail.com>
2024-12-27 07:42:36 +0100

    chore: add DataClient for in file persistance

---
+++ RSSFeedKit/Sources/FeedsFeature/Clients/DataClient.swift
@@ -0,0 +1,24 @@
+import Dependencies
+import DependenciesMacros
+import Foundation
+
+@DependencyClient
+struct DataClient: Sendable {
+    var save: @Sendable (_ data: Data, _ url: URL) throws -> Void
+    var load: @Sendable (_ url: URL) throws -> Data
+}
+
+extension DataClient: DependencyKey {
+    static let liveValue = DataClient { data, url in
+        try data.write(to: url)
+    } load: { url in
+        try Data(contentsOf: url)
+    }
+}
+
+extension DependencyValues {
+    var dataClient: DataClient {
+        get { self[DataClient.self] }
+        set { self[DataClient.self] = newValue}
+    }
+}
