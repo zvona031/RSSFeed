ec457500489b330f09829b9270e98276dcc6ed7c
Zvonimir Pavlović <zvona031@gmail.com>
2024-12-27 08:13:32 +0100

    feat: implement FileRSSFeedUrlsClient

---
+++ RSSFeedKit/Sources/FeedsFeature/Clients/FileRSSFeedUrlsClient.swift
@@ -0,0 +1,30 @@
+import Foundation
+import Dependencies
+import Domain
+
+struct FileRSSFeedURLClient {
+    private let encoder: JSONEncoder
+    private let decoder: JSONDecoder
+    private let url: URL
+    @Dependency(\.dataClient) private var dataClient
+
+    init(encoder: JSONEncoder, decoder: JSONDecoder, url: URL) {
+        self.encoder = encoder
+        self.decoder = decoder
+        self.url = url
+    }
+
+    func save(_ reminders: [RSSFeedModel]) throws {
+        let data = try encoder.encode(reminders)
+        try dataClient.save(data: data, url: url)
+    }
+
+    func load() throws -> [RSSFeedModel] {
+        guard dataClient.fileExists(at: url.path()) else {
+            return []
+        }
+        let data = try dataClient.load(url: url)
+        let reminders = try? decoder.decode([RSSFeedModel].self, from: data)
+        return reminders ?? []
+    }
+}
