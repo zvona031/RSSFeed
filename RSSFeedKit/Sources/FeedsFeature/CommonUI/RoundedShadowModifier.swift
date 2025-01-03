9ac6adb22d2bbf2091f33ab544163086938bd88b
Zvonimir Pavlović <zvona031@gmail.com>
2025-01-03 13:59:10 +0100

    chore: add custom background color that supports dark mode

--- RSSFeedKit/Sources/FeedsFeature/CommonUI/RoundedShadowModifier.swift
+++ RSSFeedKit/Sources/FeedsFeature/CommonUI/RoundedShadowModifier.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct RoundedShadowModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .background(Color.white)
+            .background(Color("itemBackground", bundle: .module))
             .cornerRadius(10)
             .shadow(color: Color.black.opacity(0.15), radius: 4, x: 0, y: 2)
     }
