a71b6586818169d6915f108c2a973cdbe3b2abd7
Zvonimir Pavlović <zvona031@gmail.com>
2025-01-06 18:03:40 +0100

    chore: move UserNotificationDelegate to the separate file

---
+++ RSSFeedKit/Sources/Clients/UserNotificationClient/UserNotificationClient.swift
@@ -0,0 +1,38 @@
+import Dependencies
+import DependenciesMacros
+import UserNotifications
+import CasePaths
+
+@DependencyClient
+public struct UserNotificationClient: Sendable {
+  public var add: @Sendable (UNNotificationRequest) async throws -> Void
+  public var getAuthorizationStatus: @Sendable () async -> UNAuthorizationStatus = { .notDetermined }
+  public var delegate: @Sendable () -> AsyncStream<DelegateEvent> = { .finished }
+  public var requestAuthorization: @Sendable (UNAuthorizationOptions) async throws -> Bool
+}
+
+extension UserNotificationClient: DependencyKey {
+    public static let liveValue = Self {
+        try await UNUserNotificationCenter.current().add($0)
+    } getAuthorizationStatus: {
+        await UNUserNotificationCenter.current().notificationSettings().authorizationStatus
+    } delegate: {
+        AsyncStream { continuation in
+            let delegate = Delegate(continuation: continuation)
+            UNUserNotificationCenter.current().delegate = delegate
+            // TODO: check this
+            continuation.onTermination = { _ in
+                _ = delegate
+            }
+        }
+    } requestAuthorization: {
+        try await UNUserNotificationCenter.current().requestAuthorization(options: $0)
+    }
+}
+
+extension DependencyValues {
+    public var userNotificationClient: UserNotificationClient {
+        get { self[UserNotificationClient.self] }
+        set { self[UserNotificationClient.self] = newValue }
+    }
+}
